87f5496f426c3a788ce7ee353af06e7782b0cfdc
Sohum Ramouthar <sohumramouthar@gmail.com>
2026-01-07 10:55:04 +0200

    Initial commit: Forex community app setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="bg-green-500 p-10">
+      <h1 className="text-6xl text-red-600 font-bold">
+        TAILwind SHOULD WORK NOW
+      </h1>
+    </div>
+  );
+}
---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,16 @@
+export default function Login ()
+{
+    return (
+
+    <div className="h-screen bg-[#0f1217]">
+        <div className="bg-[#13161c] p-8 rounded-xl w-full max-w-sm shadow-lg">
+            <h1 className="Text-5xl text-red-500 font-bold">
+            Community Login
+            </h1>
+        </div>
+    </div>
+    );
+
+
+}
+
