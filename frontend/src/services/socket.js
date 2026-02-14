b09cbde26637b3c9d285f5e8113d4aa9b773f35b
Sohum Ramouthar <sohumramouthar@gmail.com>
2026-02-14 15:24:57 +0200

    Fix socket connection for production

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,9 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io(import.meta.env.VITE_API_URL, {
   auth: {
     token: localStorage.getItem("token"),
   },
 });
+
 
 export default socket;
