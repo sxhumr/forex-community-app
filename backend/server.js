baa6f03e61e134f7be520e993582206456ae10ba
Sohum Ramouthar <sohumramouthar@gmail.com>
2026-02-18 16:41:31 +0200

    Fixed Cors Issue

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,11 @@ const server = http.createServer(app);
 // --------------------
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://forex-community-app.vercel.app",
+    ],
+    credentials: true,
   })
 );
 
@@ -49,7 +53,10 @@ app.get("/", (req, res) => {
 // --------------------
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://forex-community-app.vercel.app",
+    ],
     methods: ["GET", "POST"],
   },
 });
